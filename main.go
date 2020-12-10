2bfd658aa4f12d07542a50ecd4069af3920e530d
azimut <azimut.github@protonmail.com>
2020-12-10 13:38:21 -0300

    it renders...need to make the tree after I see how to trim messages

--- main.go
+++ main.go
@@ -6,7 +6,9 @@ import (
 	"io/ioutil"
 	"time"
 
+	markdown "github.com/MichaelMure/go-term-markdown"
 	"github.com/dustin/go-humanize"
+	"github.com/jaytaylor/html2text"
 )
 
 func human_time(unix int64) string {
@@ -56,20 +58,36 @@ func main() {
 		panic(err)
 	}
 	print_op(data, JSONFILE)
+	print_comments(data)
+}
+
+// print_comments prints the rest of the messages
+// TODO: sanitize author for trailing spaces at least
+// NOTE: Resto field is useless, it ALWAYS has the mainID
+func print_comments(data Rsp) {
+	for _, value := range data.Posts[1:] {
+		fmt.Printf("%s - %d\n", value.Author, value.No)
+		fmt.Printf("%s\n\n\n", html2console(value.Comment))
+	}
+}
+
+func html2console(raw string) string {
+	mdcomment, _ := html2text.FromString(raw, html2text.Options{PrettyTables: true})
+	return string(markdown.Render(mdcomment, 80, 3))
 }
 
 // print_op Prints the main thread post
 func print_op(resp Rsp, url string) {
 	// Header
-	fmt.Printf("\ntitle: %s\nurl: %s\navatar:%s\n\n",
+	fmt.Printf("\ntitle: %s\nurl: %s\navatar: %s\n\n",
 		resp.Posts[0].Title,
 		url,
 		resp.Posts[0].Filename+resp.Posts[0].Ext, // TODO: add domain
 	)
 	// Message
-	fmt.Printf("%s\n", resp.Posts[0].Comment)
+	fmt.Printf("%s\n", html2console(resp.Posts[0].Comment))
 	// Footer
-	fmt.Printf("\n%s - %s",
+	fmt.Printf("\n%s - %s\n\n",
 		resp.Posts[0].Author,
 		human_time(resp.Posts[0].Time))
 }
